7fcbb94b1ef5e8a4ddab0100a100502b5199e778
Esneider Serna <esneider.marulanda@gmail.com>
2017-09-26 23:55:23 -0500

    Commit

--- src/app/footer-home/footer-home.component.ts
+++ src/app/footer-home/footer-home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer-home',
   templateUrl: './footer-home.component.html',
-  styleUrls: ['./footer-home.component.css']
+  styleUrls: ['./footer-home.component.scss']
 })
 export class FooterHomeComponent implements OnInit {
 
